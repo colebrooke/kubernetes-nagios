c8d6c02f9dc41e50996ec41db289f20dcbcacf17
Timo Funke <timo.funke@biotronik.com>
2021-11-23 10:54:59 +0100

    Fix using non-unique kubectl proxy
    Fixes #20

--- check_kubernetes_api.sh
+++ check_kubernetes_api.sh
@@ -44,10 +44,11 @@ done
 if [ -z $TARGET ]; then
     type kubectl >/dev/null 2>&1 || { echo >&2 "CRITICAL: The kubectl utility is required for this script to run if no API endpoint target is specified"; exit 3; }
     TEMP_KUBECTL_LOG=$(mktemp -p /tmp check_kube_api.kubectl.log.XXXXXXXX)
-    kubectl $KUBE_CONFIG $KUBE_CONTEXT proxy > $TEMP_KUBECTL_LOG 2>&1 &
+    TEMP_KUBECTL_SOCKET=$(mktemp -up /tmp check_kube_api.kubectl.socket.XXXXXXXX)
+    kubectl $KUBE_CONFIG $KUBE_CONTEXT proxy --unix-socket=$TEMP_KUBECTL_SOCKET > $TEMP_KUBECTL_LOG 2>&1 &
     PROXY_PID=$!
     sleep 1
-    TARGET="http://127.0.0.1:8001"
+    TARGET="--unix-socket $TEMP_KUBECTL_SOCKET http://127.0.0.1"
 fi
 
 HEALTH=$(curl -sS $SSL $CREDENTIALS_FILE $TARGET/healthz)
@@ -59,14 +60,14 @@ if [ -n "$PROXY_PID" ]
 then
     kill -15 $PROXY_PID
     KUBECTL_LOG=$(cat $TEMP_KUBECTL_LOG)
-    rm $TEMP_KUBECTL_LOG
+    rm $TEMP_KUBECTL_LOG $TEMP_KUBECTL_SOCKET
 fi
 
 case "$HEALTH $BSC_HEALTH $BSR_HEALTH" in
     "ok ok ok") echo "OK - Kubernetes API status is OK" && exit 0;;
     *)
         echo "WARNING - Kubernetes API status is not OK!"
-        [ -n $KUBECTL_LOG ] && echo "Kubectl proxy log - $KUBECTL_LOG"
+        [ -n "$KUBECTL_LOG" ] && echo "Kubectl proxy log - $KUBECTL_LOG"
         echo "/healthz - $HEALTH"
         echo "/healthz/poststarthook/bootstrap-controller - $BSC_HEALTH"
         echo "/healthz/poststarthook/extensions/third-party-resources - $EXT_HEALTH"
